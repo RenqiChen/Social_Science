cc94ad1070af3fb5f143d1408e8ef01140780b0f
shy228199751@gmail.com <228199751@qq.com>
2024-09-12 12:30:41 +0800

    revise sci_team

--- sci_platform/sci_team.py
+++ sci_platform/sci_team.py
@@ -1,5 +1,5 @@
-from loguru import logger
 import os
+import logging
 from agentscope.memory import TemporaryMemory
 class Team:
     def __init__(self, team_name):
@@ -15,7 +15,7 @@ class Team:
         self.citation_id = None
         self.self_review = None
         self.paper_review = None
-        
+
         # state log
         self.state_log = {
             1: 'WAIT',
@@ -30,21 +30,23 @@ class Team:
         self.log_file = f"logs/{self.team_name}_dialogue.log"
         os.makedirs("logs", exist_ok=True)
 
-        # Remove the default terminal handler
-        logger.remove()
-
         # Check if log file exists and delete it
         if os.path.exists(self.log_file):
             os.remove(self.log_file)
 
-        logger.add(self.log_file, format="{message}", level="INFO")
-        self.logger = logger.bind(team_name=self.team_name)
+        self.logger = logging.getLogger(self.team_name)
+        self.logger.setLevel(logging.INFO)
+
+        fh = logging.FileHandler(self.log_file)
+        self.logger.addHandler(fh)
 
     def log_dialogue(self, name, content):
         self.logger.info(f'Epoch:{self.epoch} | {self.state_log[self.state]} | {name}:{content}')
 
 if __name__=='__main__':
     team1 = Team('LPL')
-    team1.state = 2
+    team2 = Team('LCK')
     team1.log_dialogue('sam', 'LPL win!')
-    team1.log_dialogue('sam', f'{team1.state}')
+    team2.log_dialogue('tom', 'LCK win!')
+    team1.log_dialogue('sam', 'LPL win again !')
+    team2.log_dialogue('tom', 'LCK win again !')
