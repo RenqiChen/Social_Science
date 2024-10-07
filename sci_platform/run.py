77dac2b84dc69be7d89bc19709aa238724756b32
RenqiChen <11910818@mail.sustech.edu.cn>
2024-10-07 11:22:34 +0800

    Delete sci_platform directory

--- sci_platform/run.py
+++
@@ -1,9 +0,0 @@
-from sci_platform import Platform
-from scientist_utils import read_txt_files_as_dict
-
-epochs = 5
-platform_example = Platform()
-platform_example.running(epochs)
-
-# paper_folder_path = "/home/bingxing2/ailab/group/ai4agr/crq/SciSci/papers"  # 替换为实际的文件夹路径
-# paper_dicts = read_txt_files_as_dict(paper_folder_path)
