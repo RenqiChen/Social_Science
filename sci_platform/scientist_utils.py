40c00cb86ff69d55e1cc69058f98f579b19f193f
RenqiChen <11910818@mail.sustech.edu.cn>
2024-10-08 15:22:08 +0800

    Update scientist_utils.py

--- sci_platform/scientist_utils.py
+++ sci_platform/scientist_utils.py
@@ -13,7 +13,7 @@ from typing import Union, Any, Sequence
 import numpy as np
 from loguru import logger
 
-# from prompt import Prompts
+from prompt import Prompts
 from agentscope.agents import AgentBase
 from agentscope.message import Msg
 import os
@@ -185,6 +185,78 @@ def format_msg(*input: Union[Msg, Sequence[Msg]]) -> list:
 
     return input_msgs
 
+# def paper_search(query : str,
+#                  top_k : int = 8,
+#                  start_year : int = None,
+#                  end_year : int = None,
+#                  search_engine : str = 'arxiv') -> list:
+#     """Given a query, retrieve k abstracts of similar papers from google scholar"""
+    
+#     proxy = {
+#         'http':'http://u-cEoRwn:EDvFuZTe@172.16.4.9:3128',
+#         'https':'http://u-cEoRwn:EDvFuZTe@172.16.4.9:3128',
+#     }
+    
+#     start_year = 0 if start_year is None else start_year
+#     end_year = 9999 if end_year is None else end_year
+#     papers = []
+#     if search_engine == 'google scholar':
+#         # retrieval_results = scholarly.search_pubs(query)
+#         retrieval_results = []
+#     elif search_engine == 'dblp':
+#         retrieval_results = dblp_search_publications(query, num_results = top_k)['content']
+#     else:
+#         temp_results = arxiv_search(query, max_results = top_k, proxy = proxy).content
+#         if isinstance(temp_results, dict):
+#             retrieval_results = temp_results['entries']
+#         else:
+#             retrieval_results = []
+#             print(temp_results)
+
+#     for paper in retrieval_results:
+
+#         if len(papers) >= top_k:
+#             break
+
+#         try:
+#             pub_year = paper.get('published', None)[:4]
+#         except:
+#             pub_year = paper.get('year', None)
+
+#         if pub_year and start_year <= int(pub_year) <= end_year:
+#             if search_engine == 'google scholar':
+#                 paper_info = {
+#                     'title': paper.get('title'),
+#                     'authors': paper.get('authors'),
+#                     'year': pub_year,
+#                     'abstract': paper.get('abstract'),
+#                     'url': paper.get('url'),
+#                     'venue': paper.get('venue')
+#                 }
+#             elif search_engine == 'dblp':
+#                 paper_info = {
+#                     'title': paper.get('title'),
+#                     'authors': paper.get('authors'),
+#                     'year': pub_year,
+#                     'abstract': paper.get('abstract'),
+#                     'url': paper.get('url'),
+#                     'venue': paper.get('venue')
+#                 }
+#             else:
+#                 paper_info = {
+#                     'title': paper.get('title'),
+#                     'authors': ','.join(paper.get('authors')),
+#                     'year': pub_year,
+#                     'abstract': paper.get('summary'),
+#                     'pdf_url': paper.get('url'),
+#                     'venue': paper.get('comment')
+#                 }
+
+#             # print(paper_info)
+#             papers.append(paper_info)
+
+#     return papers
+
 def paper_search(query : str,
                  top_k : int = 8,
                  start_year : int = None,
@@ -196,7 +268,7 @@ def paper_search(query : str,
         'http':'http://u-cEoRwn:EDvFuZTe@172.16.4.9:3128',
         'https':'http://u-cEoRwn:EDvFuZTe@172.16.4.9:3128',
     }
-    
+
     start_year = 0 if start_year is None else start_year
     end_year = 9999 if end_year is None else end_year
     papers = []
@@ -266,6 +338,7 @@ def paper_search(query : str,
 
     return papers
 
+
 # def process_file(file_path, id):
 #     try:
 #         print(id)
@@ -316,6 +389,8 @@ def read_txt_files_as_dict(folder_path):
                     file_dict={}
                     file_dict['title']=file_dict_old['title']
                     file_dict['abstract']=file_dict_old['abstract']
+                    file_dict['year']=file_dict_old['year']
+                    file_dict['citation']=file_dict_old['citation']
                     file_dict['id'] = id
                     file_dict['authors'] = None
                     file_dict['cite_papers'] = None
