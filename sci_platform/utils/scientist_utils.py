fd96b9f11a658c4b21de70f2247fa88a03381e34
RenqiChen <11910818@mail.sustech.edu.cn>
2024-10-15 17:03:04 +0800

    Update scientist_utils.py

--- sci_platform/utils/scientist_utils.py
+++ sci_platform/utils/scientist_utils.py
@@ -125,10 +125,7 @@ def team_description_detail(team: list, agent_list: list, over_state: int) -> st
             output_string += f"The Team{i} includes team members: {team_list}. "
             i=i+1
     output_string_before = f"You are currently a member of {i-1} teams. "
-    # for agent in agent_list:
-    #     if agent.name in independent_list:
-    #         output_string += f"For {agent.name}, "
-    #         output_string += convert_you_to_other(agent.sys_prompt)
+
     output_string = output_string_before + output_string + "Summarize the status of all the teams you are currently part of."
     return output_string
 
@@ -148,11 +145,6 @@ def set_parsers(
         agents = [agents]
     for agent in agents:
         agent.set_parser(parser_name)
-
-# def  update_state(team: list, agent_index: int, state: int) -> list:
-#     for team_index in team[agent_index]:
-#         team_index['state'] = state
-#     return team
 
 def format_msg(*input: Union[Msg, Sequence[Msg]]) -> list:
     """Forward the input to the model.
@@ -188,79 +180,6 @@ def formated_msg2str(input):
     for msg in input:
         input_strs.append(msg.name + ':' + msg.content)
     return '\n'.join(input_strs)
-
-# def paper_search(query : str,
-#                  top_k : int = 8,
-#                  start_year : int = None,
-#                  end_year : int = None,
-#                  search_engine : str = 'arxiv') -> list:
-#     """Given a query, retrieve k abstracts of similar papers from google scholar"""
-
-#     proxy = {
-#         'http':'http://u-cEoRwn:EDvFuZTe@172.16.4.9:3128',
-#         'https':'http://u-cEoRwn:EDvFuZTe@172.16.4.9:3128',
-#     }
-
-
-#     start_year = 0 if start_year is None else start_year
-#     end_year = 9999 if end_year is None else end_year
-#     papers = []
-#     if search_engine == 'google scholar':
-#         # retrieval_results = scholarly.search_pubs(query)
-#         retrieval_results = []
-#     elif search_engine == 'dblp':
-#         retrieval_results = dblp_search_publications(query, num_results = top_k)['content']
-#     else:
-#         temp_results = arxiv_search(query, max_results = top_k, proxy = proxy).content
-#         if isinstance(temp_results, dict):
-#             retrieval_results = temp_results['entries']
-#         else:
-#             retrieval_results = []
-#             print(temp_results)
-
-#     for paper in retrieval_results:
-
-#         if len(papers) >= top_k:
-#             break
-
-#         try:
-#             pub_year = paper.get('published', None)[:4]
-#         except:
-#             pub_year = paper.get('year', None)
-
-#         if pub_year and start_year <= int(pub_year) <= end_year:
-#             if search_engine == 'google scholar':
-#                 paper_info = {
-#                     'title': paper.get('title'),
-#                     'authors': paper.get('authors'),
-#                     'year': pub_year,
-#                     'abstract': paper.get('abstract'),
-#                     'url': paper.get('url'),
-#                     'venue': paper.get('venue')
-#                 }
-#             elif search_engine == 'dblp':
-#                 paper_info = {
-#                     'title': paper.get('title'),
-#                     'authors': paper.get('authors'),
-#                     'year': pub_year,
-#                     'abstract': paper.get('abstract'),
-#                     'url': paper.get('url'),
-#                     'venue': paper.get('venue')
-#                 }
-#             else:
-#                 paper_info = {
-#                     'title': paper.get('title'),
-#                     'authors': ','.join(paper.get('authors')),
-#                     'year': pub_year,
-#                     'abstract': paper.get('summary'),
-#                     'pdf_url': paper.get('url'),
-#                     'venue': paper.get('comment')
-#                 }
-
-#             # print(paper_info)
-#             papers.append(paper_info)
-
-#     return papers
 
 def paper_search(query : str,
                  top_k : int = 8,
@@ -343,53 +262,16 @@ def paper_search(query : str,
 
     return papers
 
-
-# def process_file(file_path, id):
-#     try:
-#         print(id)
-#         with open(file_path, 'r') as file:
-#             file_content = file.read()
-#             file_dict_old = eval(file_content)
-#             file_dict = {
-#                 'title': file_dict_old.get('title'),
-#                 'abstract': file_dict_old.get('abstract'),
-#                 'id': id,
-#                 'authors': None,
-#                 'cite_papers': None
-#             }
-#             return file_dict
-#     except json.JSONDecodeError:
-#         print(f"文件 {file_path} 的内容不是有效的JSON格式，跳过该文件。")
-#         return None
-
-# def read_txt_files_as_dict(folder_path):
-#     dict_list = []
-#     id = 0
-#     with ThreadPoolExecutor() as executor:
-#         # 只处理 .txt 文件
-#         txt_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(".txt")]
-
-#         # 并行处理文件
-#         results = list(tqdm(executor.map(lambda file: process_file(file, id), txt_files), total=len(txt_files)))
-
-#         # 过滤掉 None 结果
-#         dict_list = [res for res in results if res is not None]
-
-#     return dict_list
-
 def read_txt_files_as_dict(folder_path):
-    dict_list = []  # 用于存储所有文件的字典
+    dict_list = []  
     id = 0
-    # 遍历文件夹中所有的 .txt 文件
     for filename in tqdm(os.listdir(folder_path)):
-        if filename.endswith(".txt"):  # 确保文件是 .txt 类型
+        if filename.endswith(".txt"):  
             file_path = os.path.join(folder_path, filename)
 
-            # 打开并读取每个 .txt 文件的内容
             with open(file_path, 'r') as file:
                 file_content = file.read()
                 try:
-                    # 将内容解析为字典（假设内容是JSON格式）
                     file_dict_old = eval(file_content)
                     file_dict={}
                     file_dict['title']=file_dict_old['title']
@@ -403,7 +285,6 @@ def read_txt_files_as_dict(folder_path):
                     print(f"文件 {filename} 的内容不是有效的JSON格式，跳过该文件。")
                     continue
 
-                # 将字典添加到列表
                 dict_list.append(file_dict)
                 id = id + 1
     return dict_list
@@ -426,14 +307,14 @@ def extract_between_json_tags(text, num=None):
 
 
 def extract_metrics(text, split_keywords):
-    # 存储每个指标及其数值
+
     metrics = {}
 
     for keyword in split_keywords:
-        # 使用关键词进行分割
+
         parts = text.split(keyword)
         if len(parts) > 1:
-            # 在分割后的部分中找到第一个数字
+
             match = re.search(r'\d+', parts[1])
             if match:
                 value = int(match.group())
@@ -446,7 +327,7 @@ def extract_metrics(text, split_keywords):
     return metrics
 
 def strip_non_letters(text):
-    # 正则表达式匹配非字母字符并移除它们
+
     return re.sub(r'^[^a-zA-Z]+|[^a-zA-Z]+$', '', text)
 
 def save2database(paper_list : list[dict], output_dir : str):
@@ -509,27 +390,26 @@ def count_team(team_list: list[dict], over_state: int):
 
 
 def top_three_indices(lst):
-    # 使用enumerate获取元素及其索引，并根据元素值进行排序
+
     sorted_indices = sorted(enumerate(lst), key=lambda x: x[1], reverse=True)
 
-    # 取出前三大的元素的索引
+
     top_three = [index for index, value in sorted_indices[:3]]
 
     return top_three
 
 def extract_first_number(s):
-    # 使用正则表达式查找字符串中的第一个数字
+
     match = re.search(r'\d+', s)
     if match:
-        return match.group()  # 返回匹配到的第一个数字
-    return None  # 如果没有找到数字，返回 None
+        return match.group()  
+    return None  
 
 
 def most_frequent_element(arr):
-    # 使用 Counter 计算每个元素的出现次数
+
     count = Counter(arr)
 
-    # 返回出现次数最多的元素
     most_common_element = count.most_common(1)[0][0]
 
     return most_common_element
